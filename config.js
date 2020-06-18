3af807a94bbc0d0584194ddf9020d5025e77f4dc
jirwincline <jci@pdx.edu>
2020-06-18 02:07:56 -0700

    edit config

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Corporate Crime Control Association', // Site title.
+  siteTitleAlt: 'Corporate Crime Control', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://ccca-site2.netlify.app', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Web Presence for Corporate Crime Control Association, a premier security contracting firm based out of Oregon, USA.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  disqusShortname: 'corporate-crime-control', // Disqus shortname.
+  userName: 'Jacob Irwin-Cline',
+  userTwitter: 'na',
+  userLocation: 'Portland, Oregon',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © Corporate Crime Control 2020. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
